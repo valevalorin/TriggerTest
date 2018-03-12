d379cb6a1ca9e17cc8189e6170f18f24661e6b1f
Duncan Kelly <leucochroic@gmail.com>
2018-03-12 13:16:34 -0500

    initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject AttackGO;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Attack());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(0.5f);
+        while (true)
+        {
+            AttackGO.SetActive(true);
+            yield return new WaitForSeconds(1.0f);
+            AttackGO.SetActive(false);
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+
+
+}
